8dc63a3bc902a1e7ad25885002083b5914fe3720
IhorMasechko <igor.masechko007@gmail.com>
2023-01-17 17:03:31 +0200

    done tasks 2,3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import { throttle } from 'lodash';
 
+const blank = document.querySelector('.feedback-form');
+const email = document.querySelector('input[name="email"]');
+const message = document.querySelector('textarea[name="message"]');
+
+const LOCALSTORAGE_INFO = 'feedback-form-state';
+
+blank.addEventListener(
+  'input',
+  throttle(e => {
+    const objectToCatch = { email: email.value, message: message.value };
+    localStorage.setItem(LOCALSTORAGE_INFO, JSON.stringify(objectToCatch));
+  }, 500)
+);
+
+blank.addEventListener('submit', e => {
+  e.preventDefault();
+  console.log({ email: email.value, message: message.value });
+  blank.reset();
+  localStorage.removeItem(LOCALSTORAGE_INFO);
+});
+
+const load = key => {
+  try {
+    const dataState = localStorage.getItem(key);
+    return dataState === null ? undefined : JSON.parse(dataState);
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+};
+
+const storageBox = load(LOCALSTORAGE_INFO);
+if (storageBox) {
+  email.value = storageBox.email;
+  message.value = storageBox.message;
+}
